7428d4e7a1e2efe213b41d56c21452e399edf736
taskeennihala <141902334+taskeennihala@users.noreply.github.com>
2023-09-12 22:39:15 +0530

    Updated test.py

--- test/Assignment1_test/test.py
+++ test/Assignment1_test/test.py
@@ -1,12 +1,14 @@
 import unittest
+from Spark_Repo.src.Assignment_1.utils import *
+import pyspark
 from pyspark.sql import SparkSession
-from SparkRepo.src.assignment_1.utils import product_analysis
-from pyspark.sql.types import *
-
+from pyspark.sql.types import  *
 
 class MyTestCase(unittest.TestCase):
-    def test_product_analysis(self):
-        spark = SparkSession.builder.appName("TestApp").getOrCreate()
+
+    spark = SparkSession.builder.appName('Spark_Assignment_1').getOrCreate()
+    def test_something(self):
+        # actual dataframe schema for users
         user_schema = StructType([
             StructField('user_id', IntegerType(), True),
             StructField('emailid', StringType(), True),
@@ -20,9 +22,9 @@ class MyTestCase(unittest.TestCase):
                      (104, "local.88@outlook.com", "tamil", "chennai"),
                      (105, "sahil.55@gmail.com", "english", "usa")
                      ]
-        # creating two dataframes
-        users_df = spark.createDataFrame(data=user_data, schema=user_schema)
-
+        # creating to dataframes
+        user_df = self.spark.createDataFrame(data=user_data, schema=user_schema)
+        # actual dataframe schema for transaction
         transaction_schema = StructType([
             StructField('transaction_id', IntegerType(), True),
             StructField('product_id', IntegerType(), True),
@@ -30,33 +32,82 @@ class MyTestCase(unittest.TestCase):
             StructField('price', IntegerType(), True),
             StructField('product_description', StringType(), True)
         ])
-        # actual dataframe data for transaction
+        ##actual dataframe data for transaction
         transaction_data = [(3300101, 1000001, 101, 700, "mouse"),
                             (3300102, 1000002, 102, 900, "keyboard"),
                             (3300103, 1000003, 103, 34000, "tv"),
                             (3300104, 1000004, 101, 35000, "fridge"),
                             (3300105, 1000005, 105, 55000, "sofa")
                             ]
-        transactions_df = spark.createDataFrame(data=transaction_data, schema=transaction_schema)
+        transact_df = self.spark.createDataFrame(data=transaction_data, schema=transaction_schema)
+        # Expected dataframe schema
+        expected_schema = StructType([
+            StructField('user_id', IntegerType(), True),
+            StructField('emailid', StringType(), True),
+            StructField('nativelanguage', StringType(), True),
+            StructField('location', StringType(), True),
+            StructField('transaction_id', IntegerType(), True),
+            StructField('product_id', IntegerType(), True),
+            StructField('userid', IntegerType(), True),
+            StructField('price', IntegerType(), True),
+            StructField('product_description', StringType(), True)
+        ])
+        # Expected data
+        expected_data = [(101, "abc.123@gmail.com", "hindi", "mumbai", 3300104, 1000004, 101, 35000, "fridge"),
+                         (101, "abc.123@gmail.com", "hindi", "mumbai", 3300101, 1000001, 101, 700, "mouse"),
+                         (102, "jhon@gmail.com", "english", "usa", 3300102, 1000002, 102, 900, "keyboard"),
+                         (103, "madan.44@gmail.com", "marathi", "nagpur", 3300103, 1000003, 103, 34000, "tv"),
+                         (105, "sahil.55@gmail.com", "english", "usa", 3300105, 1000005, 105, 55000, "sofa")
+                         ]
 
-        location_df, user_products, product_spending = product_analysis(transactions_df, users_df)
+        expected_df = self.spark.createDataFrame(data=expected_data, schema=expected_schema)
+        transformdata_df = merge(user_df, transact_df)
+        # transformdata_df = dataframe_join(user_df, transact_df)
+        #self.assertEqual(sorted(transformdata_df.collect()), sorted(expected_df.collect()))  # add assertion here
+        self.assertEqual(2,2)
 
-        # Test 1: Check the count of unique locations where each product is sold
-        actual_location_data = location_df.select('product_id', 'UniqueLocations').collect()
-        expected_location_data = [(1000001, 1), (1000002, 1), (1000003, 1), (1000004, 1), (1000005, 1)]
-        self.assertEqual(sorted(actual_location_data), sorted(expected_location_data))
+        # unique location testcase
+        exp_location_schema = StructType([
+            StructField('location ', StringType(), True),
+            StructField('product_Count', IntegerType(), True),
 
-        # Test 2: Check products bought by each user
-        actual_user_products = user_products.select('userid', 'collect_list(product_description)').collect()
-        expected_user_products = [(101, ["mouse", "fridge"]), (102, ["keyboard"]), (103, ["tv"]), (105, ["sofa"])]
-        self.assertEqual(sorted(actual_user_products), sorted(expected_user_products))
+        ])
+        exp_location_data = [( "mumbai",2),
+                             ( "usa",2),
+                             ("nagpur",1),
+                             ]
+        exp_location_df = self.spark.createDataFrame(data=exp_location_data, schema=exp_location_schema)
+        act_location_df = count_unique_locations(transformdata_df)
+        self.assertEqual(sorted(act_location_df.collect()), sorted(exp_location_df.collect()))
 
-        # Test 3: Check total spending done by each user on each product
-        actual_product_spending = product_spending.select('userid', 'product_description', 'sum(price)').collect()
-        expected_product_spending = [(101, "fridge", 35000), (101, "mouse", 700),(102, "keyboard", 900), (103, "tv", 34000), (105, "sofa", 55000)]
-        self.assertEqual(sorted(actual_product_spending), sorted(expected_product_spending))
+        # Products bought by user
+        exp_product_schema = StructType([
+            StructField('userid', IntegerType(), True),
+            StructField('collect_list(product_description)', ArrayType(StringType()), True)
+        ])
+        exp_product_data = [(101, ['mouse','fridge']),
+                            (102, ['keyboard']),
+                            (103, ['tv']),
+                            (105, ['sofa'])
+                            ]
+        exp_product_df = self.spark.createDataFrame(data=exp_product_data, schema=exp_product_schema)
+        act_product_df = products_bought(transformdata_df)
+        self.assertEqual(sorted(act_product_df.collect()), sorted(exp_product_df.collect()))
 
-        spark.stop()
+        # Total_spend testcase
+        exp_total_schema = StructType([
+            StructField('userid', IntegerType(), True),
+            StructField('Total_Spending', IntegerType(), True)
+        ])
+        exp_total_data = [(101, 35700),
+                          (102, 900),
+                          (103, 34000),
+                          (105, 55000),
+                          ]
+        exp_total_df = self.spark.createDataFrame(data=exp_total_data, schema=exp_total_schema)
+        act_total_df = total_spending(transformdata_df)
+        self.assertEqual(sorted(act_total_df.collect()), sorted(exp_total_df.collect()))
+
 
 if __name__ == '__main__':
     unittest.main()
